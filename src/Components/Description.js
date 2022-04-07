edb1c9a2fd7aa59e079d5edf45df165840b1c2dc
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2022-04-07 16:43:07 +0000

    feat: Add bullet point to description

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Description = ({ desc }) => (
-  <p className="experience__description">{desc}</p>
+  <p className="experience__description">{!!desc ? <><i class='bx bxs-circle' ></i><div>{desc}</div></> : ''}</p>
 );
